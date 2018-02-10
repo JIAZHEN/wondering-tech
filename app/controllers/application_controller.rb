2a5f841fe5f5058798d910740faf9391dd036829
Joe Xie <fdf515@gmail.com>
2018-02-10 23:50:50 +0000

    Provide languages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = params[:lang] || user_preferred_language || I18n.default_locale
+  end
+
+  def default_url_options(options = {})
+    { lang: I18n.locale == I18n.default_locale ? nil : I18n.locale  }
+  end
+
+  def user_preferred_language
+    http_accept_language.compatible_language_from(I18n.available_locales)
+  end
 end
